59019fc961552b84796ffe00de67d0ceffd6d37a
Michael Ernest <m.hendersun@gmail.com>
2020-09-30 21:11:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |i| 
-    return "Hello, my name is #{i}."}
+  attendees.each do |i|
+    return "Hello, my name is #{i}."
 end

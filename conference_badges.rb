1b5f6410edb3004faeb5ca627ec75c7470c7cafa
Michael Ernest <m.hendersun@gmail.com>
2020-09-30 20:56:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,7 @@
-def conference_badges([name])
+def conference_badges(name)
   name.each do |name|
     puts "Hello, my name is #{name}"
   end
 end
 
-conference_badges("Edsger", "Ada", "Charles")
+conference_badges("Edsger")
